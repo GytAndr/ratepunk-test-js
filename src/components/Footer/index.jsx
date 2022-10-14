03d4d253c54f225f3009e422c92782ef1a928780
GytAndr <andriejunas@gmail.com>
2022-10-14 15:50:28 +0300

    stores rating added

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,6 +8,14 @@ function Footer() {
 			<section className="footer-stores">
 				<Store img="/images/chrome.svg" store="chrome web store" />
 				<Store img="/images/apple.svg" store="apple app store" />
+				<div className="footer-stores-rating">
+					<Image src="/images/star.svg" width={16} height={16} alt="" />
+					<Image src="/images/star.svg" width={16} height={16} alt="" />
+					<Image src="/images/star.svg" width={16} height={16} alt="" />
+					<Image src="/images/star.svg" width={16} height={16} alt="" />
+					<Image src="/images/star.svg" width={16} height={16} alt="" />
+					<p>Chrome Store reviews</p>
+				</div>
 			</section>
 			<section className="footer-nav"></section>
 		</footer>
