b1ec573db84fd2267e9b7fa47f09e764674d9a52
GytAndr <andriejunas@gmail.com>
2022-10-16 20:00:35 +0300

    responsive for desktop

--- src/components/LandingPg/index.jsx
+++ src/components/LandingPg/index.jsx
@@ -7,24 +7,26 @@ function LandingPg() {
 	return (
 		<main>
 			<ReferalCard />
-			<StepCard
-				img="/images/invite.svg"
-				step="STEP 1"
-				title="INVITE FRIENDS"
-				description="Refer friends with your unique referral link."
-			/>
-			<StepCard
-				img="/images/collect-coins.svg"
-				step="STEP 2"
-				title="COLLECT COINS"
-				description="Get 1 coin for each friend that installs our extension using your referral link."
-			/>
-			<StepCard
-				img="/images/voucher.svg"
-				step="STEP 3"
-				title="GET VOUCHER"
-				description="Redeem for a $20 hotel booking voucher once you collect 20 coins."
-			/>
+			<div className="step-wrapper">
+				<StepCard
+					img="/images/invite.svg"
+					step="STEP 1"
+					title="INVITE FRIENDS"
+					description="Refer friends with your unique referral link."
+				/>
+				<StepCard
+					img="/images/collect-coins.svg"
+					step="STEP 2"
+					title="COLLECT COINS"
+					description="Get 1 coin for each friend that installs our extension using your referral link."
+				/>
+				<StepCard
+					img="/images/voucher.svg"
+					step="STEP 3"
+					title="GET VOUCHER"
+					description="Redeem for a $20 hotel booking voucher once you collect 20 coins."
+				/>
+			</div>
 		</main>
 	);
 }
