d40c095c1037c1fb869f690a2b717906d9e2a644
GytAndr <andriejunas@gmail.com>
2022-10-17 20:41:04 +0300

    responsive/dynamic input added

--- src/components/LandingPg/Input.jsx
+++ src/components/LandingPg/Input.jsx
@@ -1,14 +1,15 @@
-import React, { useRef } from "react";
+import Image from "next/image";
+import React, { useEffect, useRef, useState } from "react";
 
 function Input() {
 	const emailRef = useRef();
 	const errorRef = useRef();
 	function showError(message) {
 		errorRef.current.classList.add("invalid");
-		console.log((errorRef.current.innerHTML = message));
+		errorRef.current.innerHTML = message;
 	}
+	//function to check if email format is valid/not empty
 	function checkInput() {
-		//helper function to check if email format is valid
 		function isEmailValid(email) {
 			return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
 				email
@@ -23,20 +24,60 @@ function Input() {
 			//send data to jsonbin
 		}
 	}
+	//Successful email submit component
+	function ReferralSuccess() {
+		const [buttonTxt, setbuttonTxt] = useState("Copy");
+		function toggleButtonText() {
+			window.innerWidth < 992 ? setbuttonTxt("Copy URL") : setbuttonTxt("Copy");
+		}
+		if (typeof window !== "undefined") {
+			window.addEventListener("resize", toggleButtonText);
+		}
+		return (
+			<div className="referral-input-success">
+				<div className="referral-input-success-message">
+					<Image
+						src="/images/success.svg"
+						width={24}
+						height={24}
+						alt="success icon"
+					/>
+					<p>Your email is confirmed!</p>
+				</div>
+				<div className="referral-input-success-jointBtn">
+					<input
+						className="referral-input-success-url"
+						value="https://ratepunk.com/referral"
+						readOnly
+					></input>
+					<button className="referral-btn-success">{buttonTxt}</button>
+				</div>
+			</div>
+		);
+	}
+	//Pending email validation and submition component
+	function ReferralPending() {
+		return (
+			<div className="referral-input-error">
+				<small ref={errorRef} className="referral-input-error-message">
+					Error Message
+				</small>
+				<input
+					className="referral-input"
+					type="email"
+					placeholder="Enter your email address"
+					ref={emailRef}
+				></input>
+				<button className="referral-btn" type="button" onClick={checkInput}>
+					Get Referral Link
+				</button>
+			</div>
+		);
+	}
 	return (
 		<form className="referral-input-wrapper">
-			<small ref={errorRef} className="referral-input-error">
-				Error Message
-			</small>
-			<input
-				className="referral-input"
-				type="email"
-				placeholder="Enter your email address"
-				ref={emailRef}
-			></input>
-			<button className="referral-btn" type="button" onClick={checkInput}>
-				Get Referral Link
-			</button>
+			<ReferralSuccess />
+			{/* <ReferralPending /> */}
 		</form>
 	);
 }
