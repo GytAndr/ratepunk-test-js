6e33bbe1cfe7bf2984a5151527e455b1016ee7ca
GytAndr <andriejunas@gmail.com>
2022-10-17 10:44:40 +0300

    landing and ctores footer desk responsive layout

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -8,21 +8,23 @@ function Footer() {
 	return (
 		<footer className="footer">
 			<section className="footer-stores">
-				<a
-					href="https://chrome.google.com/webstore/detail/ratepunk-same-hotel-way-c/gdaioanblcnghddimngklkhgcbomfdck?utm_source=ratepunk"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<Store img="/images/chrome.svg" store="chrome web store" />
-				</a>
-				<a
-					href="https://apps.apple.com/app/ratepunk/id1607823726"
-					target="_blank"
-					rel="noreferrer"
-				>
-					<Store img="/images/apple.svg" store="apple app store" />
-				</a>
-				<Rating />
+				<div className="footer-wrapper">
+					<a
+						href="https://chrome.google.com/webstore/detail/ratepunk-same-hotel-way-c/gdaioanblcnghddimngklkhgcbomfdck?utm_source=ratepunk"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<Store img="/images/chrome.svg" store="chrome web store" />
+					</a>
+					<a
+						href="https://apps.apple.com/app/ratepunk/id1607823726"
+						target="_blank"
+						rel="noreferrer"
+					>
+						<Store img="/images/apple.svg" store="apple app store" />
+					</a>
+					<Rating />
+				</div>
 			</section>
 			<FooterNav />
 		</footer>
