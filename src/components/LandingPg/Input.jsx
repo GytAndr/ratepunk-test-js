3401fbe3f606372f0c88fd38397004ac094971be
GytAndr <andriejunas@gmail.com>
2022-10-17 16:56:43 +0300

    email validation message and regex

--- src/components/LandingPg/Input.jsx
+++ src/components/LandingPg/Input.jsx
@@ -1,21 +1,43 @@
-import React, { useState } from "react";
+import React, { useRef } from "react";
 
 function Input() {
-	const [emailValue, setEmailValue] = useState("");
-	const onEmailChange = (e) => {
-		setEmailValue(e.target.value);
-		console.log(emailValue);
-	};
+	const emailRef = useRef();
+	const errorRef = useRef();
+	function showError(message) {
+		errorRef.current.classList.add("invalid");
+		console.log((errorRef.current.innerHTML = message));
+	}
+	function checkInput() {
+		//helper function to check if email format is valid
+		function isEmailValid(email) {
+			return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+				email
+			);
+		}
+		if (emailRef.current.value.trim() === "") {
+			showError("Email cannot be blank");
+		} else if (!isEmailValid(emailRef.current.value)) {
+			showError("Please enter valid email format");
+		} else {
+			errorRef.current.classList.remove("invalid");
+			//send data to jsonbin
+		}
+	}
 	return (
-		<>
+		<form className="referral-input-wrapper">
+			<small ref={errorRef} className="referral-input-error">
+				Error Message
+			</small>
 			<input
 				className="referral-input"
+				type="email"
 				placeholder="Enter your email address"
-				value={emailValue}
-				onChange={onEmailChange}
+				ref={emailRef}
 			></input>
-			<button className="referral-btn">Get Referral Link</button>
-		</>
+			<button className="referral-btn" type="button" onClick={checkInput}>
+				Get Referral Link
+			</button>
+		</form>
 	);
 }
 
